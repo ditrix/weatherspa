beecb1dc8f3e0a46856572c61e89c4603f285e42
ditrix <ditrix2005@gmail.com>
2019-08-14 21:24:46 +0300

    somestyles wrkjrn

---
+++ src/component/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+	return(
+       <div className="footer">
+         <a href={"mailto:dmvoloshin@gmail.com"}><h5>dmvoloshin@gmail.com</h5></a>
+         <a href={"https://openweathermap.org/api"} target={"_blank"}><h5>Open Weather</h5></a>
+        </div> 		
+	)
+}
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const OWM_API_KEY = 'bb4f8b2e6d9a8b9c9d3d77979bf6e602'
+export const GOOGLE_API_KEY = '889737625929-4qnmjcpha53jek54jsqtoakl93trh1ev'
